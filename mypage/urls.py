9c38eddfed297e95a46469db679ea1e7d5fe1962
gayeoneee <164208099+gayeoneee@users.noreply.github.com>
2024-07-04 22:51:22 +0900

    mypage update

---
+++ mypage/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'mypage'
+
+urlpatterns = [
+    path('', views.my_page, name='my_page'),
+    path('my-post/', views.my_post, name='my_post'),
+    path('my-like/', views.my_like, name='my_like'),
+    path('my-quiz', views.my_quiz, name='my_quiz'),
+    path('my-flash', views.my_flash, name='my_flash'),
+]
---
+++ mypage/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from board.models import Post
+from study.models import SubmitAnswer, SavedFlashCard
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+# 마이 페이지
+def my_page(request):
+    return render(request, 'my_page.html')
+
+# 내가 쓴 글
+@login_required(login_url='accounts:login')
+def my_post(request):
+    post_list = Post.objects.filter(author=request.user).order_by('-create_date')
+    return render(request, 'my_post.html', {'post_list':post_list})
+
+# 좋아요 한 글
+@login_required(login_url='accounts:login')
+def my_like(request):
+    post_list = Post.objects.filter(voter=request.user).order_by('-create_date')
+    return render(request, 'my_like.html', {'post_list':post_list})
+
+# 퀴즈 기록
+@login_required(login_url='accounts:login')
+def my_quiz(request):
+    answer_list = SubmitAnswer.objects.filter(user=request.user)
+    return render(request, 'my_quiz.html', {'answer_list':answer_list})
+
+# 플래시 기록
+@login_required(login_url='accounts:login')
+def my_flash(request):
+    card_list = SavedFlashCard.objects.filter(user=request.user)
+    return render(request, 'my_flash.html', {'card_list':card_list})
